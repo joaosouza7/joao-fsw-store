f4d34731025f94bec099039f1ab0b6188488ddfb
Joao Souza <joaoosouza07@gmail.com>
2023-10-18 11:34:56 -0300

    :sparkles: feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        width={0}
+        height={0}
+        className="mt-5 h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   );
 }
