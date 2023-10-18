d6f22decdea76f3417cf109c2ad2e60edcddd78d
Joao Souza <joaoosouza07@gmail.com>
2023-10-18 11:52:16 -0300

    :sparkles: feat: add third promo banner to home page

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -5,7 +5,7 @@ const PromoBanner = ({ alt, ...props }: ImageProps) => {
     <Image
       width={0}
       height={0}
-      className="mt-5 h-auto w-full px-5"
+      className="h-auto w-full px-5"
       sizes="100vw"
       alt={alt}
       {...props}
